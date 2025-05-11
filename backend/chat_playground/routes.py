3a9a96ca4a7b94fe8f78945d41d2034cd3a7f628
Waynelle Ize <waynelleize@dhcp-v080-187.mobile.uci.edu>
2025-05-11 11:01:00 -0700

    tracking down a 500

--- backend/chat_playground/routes.py
+++ backend/chat_playground/routes.py
@@ -5,16 +5,22 @@ from . import services
 
 router = APIRouter()
 
+@router.post("/chat/model/chat/anthropic.claude-v2/invoke")
 
-@router.post("/foundation-models/model/chat/anthropic.claude-v2/invoke")
 def invoke(body: models.ChatRequest):
+    print("didnt even make it past post u get the prompt?")
+
     try:
+        print("did u get the prompt? Prompt:"+ body.prompt)
         completion = services.invoke(body.prompt)
+        print("did u get the ans? Response:"+ completion)
         return models.ChatResponse(
             completion=completion
         )
     except ClientError as e:
         if e.response["Error"]["Code"] == "AccessDeniedException":
             raise HTTPException(status_code=403)
+        elif e.response["Error"]["Code"] == "ValidationException":
+            raise HTTPException(status_code=422)
         else:
             raise HTTPException(status_code=500)
